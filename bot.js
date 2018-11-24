0981c4344148b77a20d386144742e7300af49b11
TGYT <41660086+TGYT@users.noreply.github.com>
2018-11-24 16:03:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,24 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
 console.log('By : m7md');
-client.user.setGame(argresult, "https://www.twitch.tv/M3roof");
+client.user.setPresence({
+  status: 'dnd',
+  game: { 
+     type: 0,
+     name: 'هاي يوتيوووب',
+     details: `هاي يوتيوب`,
+     url: 'http://twitch.tv/M7md_Salih',
+     state: `هاي يوتيوب`,
+    application_id: '377479790195769345',
+     assets: {
+        small_image: `377480550207717376`,
+        small_text: 'هاي يوتيوب',
+        large_image: `377480353259978752`,
+        large_text: `هاي يوتيوب` }
+
+  }
+    });
+});
   client.user.setPresence({
   game: { 
      type: 0,
