bd70f7f899d92270cf04e43be195f4bdfefbc36b
TGYT <41660086+TGYT@users.noreply.github.com>
2018-11-24 15:29:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,19 +6,19 @@ const client = new Discord.Client();
 client.on("ready", () => {
 console.log('By : m7md');
 client.user.setPresence({
-  status: 'dnd',
+  status: 'streaming',
   game: { 
      type: 0,
-     name: 'هاي يوتيوووب',
-     details: `هاي يوتيوب`,
+     name: 'Fu*king Girls.',
+     details: 'Still One To Fu*k.`,
      url: 'http://twitch.tv/M7md_Salih',
-     state: `هاي يوتيوب`,
+     state: `Fu*king Girls.`,
     application_id: '377479790195769345',
      assets: {
-        small_image: `377480550207717376`,
-        small_text: 'هاي يوتيوب',
-        large_image: `377480353259978752`,
-        large_text: `هاي يوتيوب` }
+        small_image: `515880221216342018`,
+        small_text: 'Get Out If Here.',
+        large_image: `515879640905154561`,
+        large_text: `Hot.` }
 
   }
     });
