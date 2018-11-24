dc7acafd487621eed2bd0c51a8d5c2aa805b2462
TGYT <41660086+TGYT@users.noreply.github.com>
2018-11-24 16:44:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,24 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on("ready", () => {
 console.log('By : m7md');
-client.user.setGame(argresult, "https://www.twitch.tv/M3roof");    
-client.user.setPresence({
-  status: 'dnd',
-  game: { 
-     type: 0,
-     name: 'هاي يوتيوووب',
-     details: `هاي يوتيوب`,
-     url: 'http://twitch.tv/M7md_Salih',
-     state: `هاي يوتيوب`,
-    application_id: '377479790195769345',
-     assets: {
-        small_image: `377480550207717376`,
-        small_text: 'هاي يوتيوب',
-        large_image: `377480353259978752`,
-        large_text: `هاي يوتيوب` }
+client.user.setGame("Hello", "https://www.twitch.tv/M3roof");    
 
-  }
-    });
-});
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
