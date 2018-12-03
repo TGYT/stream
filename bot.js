1743be8b099c6f31538bafeca6797511f0b09568
TGYT <41660086+TGYT@users.noreply.github.com>
2018-12-03 10:46:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,28 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+
+
+
+client.on("ready", () => {
+console.log('By : Slayers');
+client.user.setPresence({
+  status: 'dnd',
+  game: { 
+     type: 0,
+     name: '.وِمعظٌمۂم ليّسً لۂ محًل منٌ ٱلٱعرٱبّ',
+  
+     url: 'https://m.twitch.tv/twitchpresents',
+     state: `ٱبّدُأ بّنٌفُسًك,يّتُغّيّر ٱلكوِنٌ منٌ حًوِلك`,
+    application_id: '377479790195769345',
+     assets: {
+        small_image: `377480550207717376`,
+        small_text: 'فضولي 😕',
+        large_image: `377480353259978752`,
+        large_text: `خليك في نفسك😑` }
+
+  }
+    });
 });
 
-
-const developers = ["279221610408312834"]
-const adminprefix = "$";
-
-client.user.setGame(".وِمعظٌمۂم ليّسً لۂ محًل منٌ ٱلٱعرٱبّ", "https://m.twitch.tv/twitchpresents");
 
 client.login(process.env.BOT_TOKEN);
